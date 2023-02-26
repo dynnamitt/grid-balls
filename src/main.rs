186ce9e8bbbecfe6fa2ca1688eef2f988eb4645c
kjetil midtlie <kjetil.midtlie@gmail.com>
2023-02-26 11:20:34 +0100

    tutorial running fine, bevy

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use bevy::prelude::*;
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
+    }
+}
+
+fn hello_world() {
+    println!("Hello welt");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+/*
+                 _
+ _ __ ___   __ _(_)_ __
+| '_ ` _ \ / _` | | '_ \
+| | | | | | (_| | | | | |
+|_| |_| |_|\__,_|_|_| |_|
+
+*/
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins) // PluginGroup
+        .add_plugin(HelloPlugin) // single
+        .run();
+}
